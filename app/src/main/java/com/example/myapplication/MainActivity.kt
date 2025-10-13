7df6423300ec00f7654ddb428618ccf9241d8c18
Voynich3529 <237416236+DavidTOuyang@users.noreply.github.com>
2025-10-13 10:59:49 -0600

    starting with the kt development

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -5,16 +5,33 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.myapplication.databinding.ActivityMainBinding
+import android.widget.Toast
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // variables from xml
+        val myRecyclerView = binding.recyclerView
+        val myWelcomeText = binding.welcomeText
+        val myEditText = binding.messageEditText
+        val mySendButton = binding.sendButton
+
+        mySendButton.setOnClickListener {
+            val question = myEditText.text.toString().trim()
+
+            if (question.isNotEmpty()) {
+                Toast.makeText(this, question, Toast.LENGTH_LONG).show()
+            }
         }
     }
 }
