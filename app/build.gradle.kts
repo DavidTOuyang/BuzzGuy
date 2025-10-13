fc4a9da2f3b82b1cc2fa455bb203bc3000a866b0
Voynich3529 <237416236+DavidTOuyang@users.noreply.github.com>
2025-10-12 18:07:11 -0600

    added FireStore and Auth

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,6 +46,13 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.firebase.crashlytics)
+    // Import the Firebase BoM
+    implementation(platform(libs.firebase.bom))
+
+    // Add the specific dependencies using aliases
+    implementation(libs.firebase.auth)
+    implementation(libs.firebase.firestore)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
